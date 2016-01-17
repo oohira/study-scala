f3e74f1a00e2ef009ef815a64f4265d1e600d2d8
oohira <oohira.dev@gmail.com>
2016-01-17 22:04:52 +0900

    ユーザー一覧の表示

--- bizreach-play2-hands-on/play2-hands-on/app/controllers/UserController.scala
+++ bizreach-play2-hands-on/play2-hands-on/app/controllers/UserController.scala
@@ -2,15 +2,24 @@ package controllers
 
 import javax.inject.Inject
 
-import play.api.db.slick.{HasDatabaseConfigProvider, DatabaseConfigProvider}
+import models.Tables._
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.mvc.Controller
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.mvc.{Action, Controller}
 import slick.driver.JdbcProfile
+import slick.driver.H2Driver.api._
 
 class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider, val messagesApi: MessagesApi)
   extends Controller with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
 
-  def list = TODO
+  def list = Action.async { implicit rs =>
+    // IDの昇順にすべてのユーザ情報を取得
+    db.run(Users.sortBy(t => t.id).result).map { users =>
+      // 一覧画面を表示
+      Ok(views.html.user.list(users));
+    }
+  }
 
   def edit(id: Option[Long]) = TODO
 
