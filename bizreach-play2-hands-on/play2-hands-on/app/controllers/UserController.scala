b958e19a7f39cf993dc70318879c480feecb2920
oohira <oohira.dev@gmail.com>
2016-01-18 22:33:17 +0900

    ユーザー登録・編集画面

--- bizreach-play2-hands-on/play2-hands-on/app/controllers/UserController.scala
+++ bizreach-play2-hands-on/play2-hands-on/app/controllers/UserController.scala
@@ -2,13 +2,33 @@ package controllers
 
 import javax.inject.Inject
 
+import controllers.UserController._
 import models.Tables._
+import play.api.data.Form
+import play.api.data.Forms._
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.mvc.{Action, Controller}
-import slick.driver.JdbcProfile
 import slick.driver.H2Driver.api._
+import slick.driver.JdbcProfile
+
+import scala.concurrent.Future
+
+object UserController {
+
+  // フォームの値を格納するケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // formから送信されたデータ ⇔ ケースクラスの変換を行う
+  val userForm = Form(
+    mapping(
+      "id" -> optional(longNumber),
+      "name" -> nonEmptyText(maxLength = 20),
+      "companyId" -> optional(number)
+    )(UserForm.apply)(UserForm.unapply)
+  )
+}
 
 class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider, val messagesApi: MessagesApi)
   extends Controller with HasDatabaseConfigProvider[JdbcProfile] with I18nSupport {
@@ -21,7 +41,24 @@ class UserController @Inject()(val dbConfigProvider: DatabaseConfigProvider, val
     }
   }
 
-  def edit(id: Option[Long]) = TODO
+  def edit(id: Option[Long]) = Action.async { implicit rs =>
+    val form = if (id.isDefined) {
+      // リクエストパラメータにIDが存在する場合
+      db.run(Users.filter(t => t.id === id.get.bind).result.head).map { user =>
+        userForm.fill(UserForm(Some(user.id), user.name, user.companyId))
+      }
+    } else {
+      // リクエストパラメータにIDが存在しない場合
+      Future {
+        userForm
+      }
+    }
+    form.flatMap { form =>
+      db.run(Companies.sortBy(_.id).result).map { companies =>
+        Ok(views.html.user.edit(form, companies))
+      }
+    }
+  }
 
   def create = TODO
 
