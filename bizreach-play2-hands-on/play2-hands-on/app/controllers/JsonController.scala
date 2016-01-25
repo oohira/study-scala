0384c9605ebe607e0bbf6e0360b3cfabe0f1cfb7
oohira <oohira.dev@gmail.com>
2016-01-25 23:37:56 +0900

    ユーザー一覧取得APIの実装

--- bizreach-play2-hands-on/play2-hands-on/app/controllers/JsonController.scala
+++ bizreach-play2-hands-on/play2-hands-on/app/controllers/JsonController.scala
@@ -2,17 +2,42 @@ package controllers
 
 import javax.inject.Inject
 
+import models.Tables._
 import play.api.db.slick._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.libs.json.{Writes, JsValue, Json}
 import play.api.mvc._
+import slick.driver.H2Driver.api._
 import slick.driver.JdbcProfile
+
+object JsonController {
+  // UsersRowをJSONに変換するためのWritesを定義
+  implicit val usersRowWritesFormat = new Writes[UsersRow]{
+    def writes(user: UsersRow): JsValue = {
+      Json.obj(
+        "id"        -> user.id,
+        "name"      -> user.name,
+        "companyId" -> user.companyId
+      )
+    }
+  }
+}
 
 class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider) extends Controller
   with HasDatabaseConfigProvider[JdbcProfile] {
 
+  import JsonController._
+
   /**
     * 一覧表示
     */
-  def list = TODO
+  def list = Action.async { implicit rs =>
+    // IDの昇順にすべてのユーザ情報を取得
+    db.run(Users.sortBy(t => t.id).result).map { users =>
+      // ユーザの一覧をJSONで返す
+      Ok(Json.obj("users" -> users))
+    }
+  }
 
   /**
     * ユーザ登録
