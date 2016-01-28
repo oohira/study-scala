4d691b9445b84b6be7290e834c38aad686d3e092
oohira <oohira.dev@gmail.com>
2016-01-28 23:55:15 +0900

    ユーザー登録・更新APIの実装

--- bizreach-play2-hands-on/play2-hands-on/app/controllers/JsonController.scala
+++ bizreach-play2-hands-on/play2-hands-on/app/controllers/JsonController.scala
@@ -5,10 +5,12 @@ import javax.inject.Inject
 import models.Tables._
 import play.api.db.slick._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json.{Writes, JsValue, Json}
+import play.api.libs.json._
 import play.api.mvc._
 import slick.driver.H2Driver.api._
 import slick.driver.JdbcProfile
+
+import scala.concurrent.Future
 
 object JsonController {
   // UsersRowをJSONに変換するためのWritesを定義
@@ -19,6 +21,20 @@ object JsonController {
         "name"      -> user.name,
         "companyId" -> user.companyId
       )
+    }
+  }
+
+  // ユーザ情報を受け取るためのケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // JSONをUserFormに変換するためのReadsを定義
+  implicit val userFormFormat = new Reads[UserForm]{
+    override def reads(js: JsValue): JsResult[UserForm] = {
+      JsSuccess(UserForm(
+        id        = (js \ "id"       ).asOpt[Long],
+        name      = (js \ "name"     ).as[String],
+        companyId = (js \ "companyId").asOpt[Int]
+      ))
     }
   }
 }
@@ -42,12 +58,38 @@ class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider) ext
   /**
     * ユーザ登録
     */
-  def create = TODO
+  def create = Action.async(parse.json) { implicit rs =>
+    rs.body.validate[UserForm].map { form =>
+      // OKの場合はユーザを登録
+      val user = UsersRow(0, form.name, form.companyId)
+      db.run(Users += user).map { _ =>
+        Ok(Json.obj("result" -> "success"))
+      }
+    }.recoverTotal { e =>
+      // NGの場合はバリデーションエラーを返す
+      Future {
+        BadRequest(Json.obj("result" ->"failure", "error" -> JsError.toJson(e)))
+      }
+    }
+  }
 
   /**
     * ユーザ更新
     */
-  def update = TODO
+  def update = Action.async(parse.json) { implicit rs =>
+    rs.body.validate[UserForm].map { form =>
+      // OKの場合はユーザ情報を更新
+      val user = UsersRow(form.id.get, form.name, form.companyId)
+      db.run(Users.filter(t => t.id === user.id.bind).update(user)).map { _ =>
+        Ok(Json.obj("result" -> "success"))
+      }
+    }.recoverTotal { e =>
+      // NGの場合はバリデーションエラーを返す
+      Future {
+        BadRequest(Json.obj("result" ->"failure", "error" -> JsError.toJson(e)))
+      }
+    }
+  }
 
   /**
     * ユーザ削除
